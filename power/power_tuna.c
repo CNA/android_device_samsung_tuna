b5282efd34ac6a05869d08e7054512cf022c575d
Codename Anroid <codenamegit@gmail.com>
2012-10-03 21:20:47 -0500

    new logic to fix 700mhz on screen on

--- power/power_tuna.c
+++ power/power_tuna.c
@@ -25,7 +25,15 @@
 #include <hardware/hardware.h>
 #include <hardware/power.h>
 
+#define SCALINGMAXFREQ_PATH "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq"
+#define SCREENOFFMAXFREQ_PATH "/sys/devices/system/cpu/cpu0/cpufreq/screen_off_max_freq"
 #define BOOSTPULSE_PATH "/sys/devices/system/cpu/cpufreq/interactive/boostpulse"
+
+#define MAX_BUF_SZ	10
+
+/* initialize to something safe */
+static char screen_off_max_freq[MAX_BUF_SZ] = "700000";
+static char scaling_max_freq[MAX_BUF_SZ] = "1200000";
 
 struct tuna_power_module {
     struct power_module base;
@@ -33,9 +41,6 @@ struct tuna_power_module {
     int boostpulse_fd;
     int boostpulse_warned;
 };
-
-/* Stored values, so we don't overwrite them when switching the screen on and off */
-static char max_freq_on[20];
 
 static void sysfs_write(char *path, char *s)
 {
@@ -58,18 +63,21 @@ static void sysfs_write(char *path, char *s)
     close(fd);
 }
 
-static int sysfs_read(char *path, char *s, int len)
+int sysfs_read(const char *path, char *buf, size_t size)
 {
-    char buf[80];
-    int fd = open(path, O_RDONLY);
+	int fd, len;
 
-    if (fd < 0) {
-        strerror_r(errno, buf, sizeof(buf));
-        ALOGE("Error opening %s: %s\n", path, buf);
-        return -1;
-    }
+	fd = open(path, O_RDONLY);
+	if (fd < 0)
+		return -1;
 
-    return read(fd, s, len);
+	do {
+		len = read(fd, buf, size);
+	} while (len < 0 && errno == EINTR);
+
+	close(fd);
+
+	return len;
 }
 
 static void tuna_power_init(struct power_module *module)
@@ -115,18 +123,24 @@ static int boostpulse_open(struct tuna_power_module *tuna)
 
 static void tuna_power_set_interactive(struct power_module *module, int on)
 {
-    if (!on) {
-        sysfs_read("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq",
-                max_freq_on, sizeof(max_freq_on));
-    }
+    int len;
+    char buf[MAX_BUF_SZ];
 
     /*
      * Lower maximum frequency when screen is off.  CPU 0 and 1 share a
      * cpufreq policy.
      */
+    if (!on) {
+        /* read the current scaling max freq and save it before updating */
+        len = sysfs_read(SCALINGMAXFREQ_PATH, buf, sizeof(buf));
 
-    sysfs_write("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq",
-                on ? max_freq_on : "700000");
+        if (len != -1)
+            memcpy(scaling_max_freq, buf, sizeof(buf));
+
+        sysfs_write(SCALINGMAXFREQ_PATH, screen_off_max_freq);
+    }
+    else 
+        sysfs_write(SCALINGMAXFREQ_PATH, scaling_max_freq);
 }
 
 static void tuna_power_hint(struct power_module *module, power_hint_t hint,
